44c95497221909fde2dc89ba5d2c94fc74ef7dae
Yannick Weiss <hey@yannick.dev>
2023-06-17 18:59:31 +0200

    Removed embed version and fixed test

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+set -e
 export GIT_COMMIT=$(git rev-parse HEAD)
 export INPUT_VERSION=0.1.0
-go build -ldflags "-X main.BuildVersion=$INPUT_VERSION -X main.GitCommit=$GIT_COMMIT"
+go build -ldflags "-X main.buildVersion=$INPUT_VERSION -X main.gitCommit=$GIT_COMMIT"
 ./github-go-release-action version
 ./github-go-release-action
 
